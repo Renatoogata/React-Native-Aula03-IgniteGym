70316d6eee63e5f62d255a356a624c12134f8200
Renato Ide Ogata <renatoogata95@gmail.com>
2023-04-06 19:53:32 -0300

    image profile update

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { Center, ScrollView, VStack, Skeleton, Text, Heading } from "native-base"; //Skeleton é uma mascara para quando um componente nãoe estiver sido carregado mostrar ele
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast } from "native-base"; //Skeleton é uma mascara para quando um componente nãoe estiver sido carregado mostrar ele
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -11,6 +13,47 @@ const PHOTO_SIZE = 33; // como o skeleton e a propria foto de perfil utilizam o 
 
 export function Profile() {
     const [photoIsLoading, setPhotoIsLoading] = useState(false); // esse estado representa se a photo está carregando ou não
+    const [userPhoto, setUserPhoto] = useState('https://img.quizur.com/f/img628d02b79ce434.31201082.jpg?lastEdited=1653408443')
+
+    const toast = useToast()
+
+    async function handleUserPhotoSelect() {
+        setPhotoIsLoading(true) // ativar o skeleton
+        try {
+            const photoSelect = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images, // tipo de conteudo que vai ser aceito da galeria do usuario
+                quality: 1, // qualidade vai de 0 a 1,
+                aspect: [4, 4], //deixar a imagem quadrada
+                allowsEditing: true // recortar a imagem 
+            });
+
+            if (photoSelect.canceled) {
+                return; // quando o usuario cancela o upload de imagem essa propriedade retorna true e a gente pode parar a função
+            }
+
+            if (photoSelect.assets[0].uri) {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelect.assets[0].uri) //pega as informações da foto (local, tamanho etc)
+
+                if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 0.1) { // transformar de bytes pra megabytes
+                    return toast.show({
+                        title: 'Tamanho da Imagem Invalido',
+                        _title: { alignSelf: 'center' },
+                        description: 'Essa Imagem é muito grande. Escolha uma imagem de até 5 MB',
+                        placement: 'top',
+                        bgColor: 'red.500'
+                    })
+                }
+
+                setUserPhoto(photoSelect.assets[0].uri)
+            }
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setPhotoIsLoading(false)
+        }
+
+    }
+
 
     return (
         <VStack flex={1}>
@@ -33,12 +76,12 @@ export function Profile() {
                             />
                             :
                             <UserPhoto
-                                source={{ uri: 'https://img.quizur.com/f/img628d02b79ce434.31201082.jpg?lastEdited=1653408443' }}
+                                source={{ uri: userPhoto }}
                                 alt="Foto do usuário"
                                 size={PHOTO_SIZE}
                             />
                     }
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleUserPhotoSelect}>
                         <Text
                             color="green.500"
                             fontWeight="bold"

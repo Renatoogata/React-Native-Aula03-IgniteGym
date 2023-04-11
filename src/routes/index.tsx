7bd30da20c3ab1d0de8b84f457abf9824c972afd
Renato Ide Ogata <renatoogata95@gmail.com>
2023-04-11 14:59:36 -0300

    creating hook for context

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,17 @@ import { useContext } from "react";
 import { useTheme, Box } from "native-base"; //pegar o thema padrão definido no App.tsx
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native"; // DefaultTheme como o nome sugere definir um tema padrão
 
-import { AuthRoutes } from "./auth.routes";
-import { AppRoutes } from "./app.routes";
-import { AuthContext } from "@contexts/AuthContext";
+import { useAuth } from "@hooks/useAuth";
 
+import { AuthRoutes } from "./auth.routes";
+
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
     const { colors } = useTheme();
+    const { user } = useAuth();
 
-    const contextData = useContext(AuthContext)
-    console.log("Usuario logado ->", contextData)
+    console.log("Usuario logado ->", user)
 
     const theme = DefaultTheme;
     theme.colors.background = colors.gray[700];
